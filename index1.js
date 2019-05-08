6528d37f9d7d01cb7d766e56fdbee3d4668c96b9
wangyuixang <651033260@qq.com>
2019-05-08 23:17:37 +0800

    add

--- index1.js
+++ index1.js
@@ -33,7 +33,7 @@ var top250 = {
         _this.isLoading = true
         _this.$element.find('.loading').show()
         $.ajax({
-            url: 'http://api.douban.com/v2/movie/top250',
+            url: 'https://api.douban.com/v2/movie/top250',
             data: {
                 start: _this.index || 0
             },
@@ -121,7 +121,7 @@ var usBox = {
       _this.isLoading = true
       _this.$element.find('.loading').show()
       $.ajax({
-        url: 'http://api.douban.com/v2/movie/us_box',
+        url: 'https://api.douban.com/v2/movie/us_box',
         dataType: 'jsonp'
       }).done(function(ret){
         callback&&callback(ret)
@@ -205,7 +205,7 @@ var search = {
       var _this = this
       _this.$element.find('.loading').show()
       $.ajax({
-        url: 'http://api.douban.com/v2/movie/search',
+        url: 'https://api.douban.com/v2/movie/search',
         data: {
           q: _this.keyword
         },
