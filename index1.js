9c7ef855f2f427cb45cea5c6ae8c4734648f8e40
wyx318 <33472703+wyx318@users.noreply.github.com>
2019-11-24 00:19:09 +0800

    Update index1.js

--- index1.js
+++ index1.js
@@ -125,7 +125,7 @@ var usBox = {
       _this.isLoading = true
       _this.$element.find('.loading').show()
       $.ajax({
-        url: ''http://api.douban.com/v2/movie/us_box?apikey=0df993c66c0c636e29ecbb5344252a4a',
+        url: 'http://api.douban.com/v2/movie/us_box?apikey=0df993c66c0c636e29ecbb5344252a4a',
         dataType: 'jsonp'
       }).done(function(ret){
         callback&&callback(ret)
